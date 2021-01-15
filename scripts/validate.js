7bf267e135b6336327f50ca0af0c674147c4373f
urakaev <urak.bogd23@gmail.com>
2021-01-15 16:33:56 +0300

    remove console.log

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,72 +1,4 @@
-
-/*const showInputError = (form, input, config) => {
-    const error = form.querySelector(`#${input.id}-error`);
-    error.textContent = input.validationMessage;
-    input.classList.add(config.inputErrorClass)
-}
-
-const hideInputError = (form, input, config) => {
-    const error = form.querySelector(`#${input.id}-error`);
-    error.textContent = '';
-    input.classList.remove(config.inputErrorClass)
-}
-
-const checkInputValidity = (form, input, config) => {
-    !input.validity.valid ? showInputError(form, input, config) : hideInputError(form, input, config)
-}
-
-const setButtonState = (button, isActive, config) => {
-    if(isActive) {
-        button.classList.remove(config.inactiveButtonClass);
-        button.disabled = false;
-    }
-    else {
-        button.classList.add(config.inactiveButtonClass);
-        button.disabled = true;
-    }
-}
-
-
-const setEventListeners = (form, config) => {
-    const inputsList = form.querySelectorAll(config.inputSelector);
-    const submitButton = form.querySelector(config.submitButtonSelector);
-
-    inputsList.forEach((input) => {
-        input.addEventListener('input', () => {
-            checkInputValidity(form, input, config);
-            setButtonState(submitButton, form.checkValidity(), config);
-        })
-
-    })
-}
-
-const enableValidation = (config) => {
-    const forms = document.querySelectorAll(config.formSelector);
-    forms.forEach((form) => {
-        setEventListeners(form, config);
-
-        const submitButton = form.querySelector(config.submitButtonSelector);
-
-        form.addEventListener('submit', (evt) => {
-            evt.preventDefault();
-            setButtonState(submitButton, false, config);
-        })
-
-
-        setButtonState(submitButton, form.checkValidity(), config);
-    })
-}
-
-enableValidation({
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__submit-button',
-    inactiveButtonClass: 'popup__submit-button_state_disActive',
-    inputErrorClass: 'popup__input_state_invalid',
-    errorClass: 'popup__input-error'
-  }); */
-
-  class FormValidator {
+class FormValidator {
     constructor(config, formElement) {
       this._config = config;
       this._selector = formElement;
@@ -112,7 +44,6 @@ enableValidation({
     }
 
     enableValidation() {
-      console.log(this._selector)
       this._setEventListeners(this._selector, this._config);
 
       const submitButton = this._selector.querySelector(this._config.submitButtonSelector);
@@ -140,7 +71,6 @@ document.querySelectorAll('.popup__form').forEach((item) => {
     errorClass: 'popup__input-error'
   }
   const validator = new FormValidator(config, item);
-  console.log(validator)
 
   validator.enableValidation()
 })
